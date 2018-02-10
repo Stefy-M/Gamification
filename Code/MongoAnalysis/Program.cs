45156a3a46156744d7d81d8c964a501c152a2f6f
nhaney <nhaney@eecs.wsu.edu>
2018-02-09 21:13:41 -0800

    mongo to c#
    tool that gets player info from server and outputs to file

--- Code/MongoAnalysis/Program.cs
+++ Code/MongoAnalysis/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using MongoDB.Bson
+using MongoDB.Driver
+
 
 namespace MongoAnalysis
 {
+
     class Program
     {
+		private string connectionString = "mongodb://auser:g0HUsk!E$@ccain.eecs.wsu.edu:441";
         static void Main(string[] args)
         {
-            
+            var client = new MongoClient(connectionString);
+			var database = client.getDatabase("PlayersV2");
+
         }
     }
 }
---
+++ Code/mongotocsharp/MongoAnalysis/MongoAnalysis/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+
+namespace MongoAnalysis
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task newTask =  Program.MainAsync(args);
+            newTask.Wait();
+
+        }
+        static async Task MainAsync(string[] args)
+        {
+            string connectionString = "mongodb://ccain.eecs.wsu.edu:443/admin";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("test");
+            bool isMongoLive = database.RunCommandAsync((Command<BsonDocument>)"{ping:1}").Wait(1000);
+            if(isMongoLive)
+            {
+                Console.WriteLine("Connected!");
+            }
+            else
+            {
+                Console.WriteLine("Could not connect");
+
+            }
+            var collection = database.GetCollection<BsonDocument>("playersV2");
+
+            using (IAsyncCursor<BsonDocument> cursor = await collection.FindAsync(new BsonDocument()))
+                while (await cursor.MoveNextAsync())
+                {
+                    IEnumerable<BsonDocument> batch = cursor.Current;
+                    foreach (BsonDocument document in batch)
+                    {
+                        Console.WriteLine(document);
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+}
+
+
+
