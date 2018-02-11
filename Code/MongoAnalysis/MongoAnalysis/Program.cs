eeb3cb91b1a393c9e5d41c6dd8c95deee50a44db
ktran <kevin.p.tran@wsu.edu>
2018-02-10 16:53:07 -0800

    add output of number of players during an hour

--- Code/MongoAnalysis/MongoAnalysis/Program.cs
+++ Code/MongoAnalysis/MongoAnalysis/Program.cs
@@ -114,6 +114,70 @@ namespace MongoAnalysis
 					++numActive;
 			Console.WriteLine("Active Players/Accounts = {0}/{1}({2}%)",
 				numActive, players.Count, 100 * numActive / players.Count);
+
+
+			// Rough code for finding # of active players during hours/days
+			bool dateTimesOverlap(DateTime a1, DateTime a2, DateTime b1, DateTime b2)
+			{
+				return (b1 < a2 && b2 > a1);
+			}
+
+			bool loginTimesOverlap(Login l1, Login l2)
+			{
+				return dateTimesOverlap(l1.LoginTime, l1.LogoutTime,
+					l2.LoginTime, l2.LogoutTime);
+			}
+
+			int numDaysToLog = 30;
+			var loginTimeSegments = new List<Login>();
+			var numActivePlayersSegments = new List<int>();
+			for (int d = 0; d < numDaysToLog; ++d)
+				for (int h = 0; h < 24; ++h)
+					loginTimeSegments.Add(new Login
+					{
+						LoginTime = DateTime.Today.AddDays(1).Subtract(new TimeSpan(d, h + 1, 0, 0, 0)),
+						LogoutTime = DateTime.Today.AddDays(1).Subtract(new TimeSpan(d, h, 0, 0, 1))
+					});
+
+			Console.WriteLine("\n\nNumber of people logged in during the hour:");
+			for (int i = 0; i < loginTimeSegments.Count; ++i)
+			{
+				int numActivePlayers = 0;
+				foreach (var p in players)
+				{
+					bool wasActive = false;
+					foreach (var l in p.Logins)
+					{
+						wasActive = wasActive || loginTimesOverlap(l, loginTimeSegments[i]);
+						if (wasActive) break;
+					}
+					if (wasActive) numActivePlayers++;
+				}
+				numActivePlayersSegments.Add(numActivePlayers);
+			}
+
+			Console.Write(" Hour Slot | ");
+			for (int i = 0; i < 10; ++i)
+				Console.Write(" " + i + " ");
+			for (int i = 10; i < 24; ++i)
+				Console.Write(i + " ");
+			Console.Write("\n------------");
+			for (int i = 0; i < 24; ++i)
+				Console.Write("---");
+			Console.WriteLine();
+
+			for (int d = 0; d < numDaysToLog; ++d)
+			{
+				Console.Write(loginTimeSegments[d * 24].LoginTime.ToShortDateString() + " | ");
+				for (int h = 23; h >= 0; --h)
+				{
+					if (loginTimeSegments[d * 24 + h].LoginTime > DateTime.Now)
+						Console.Write(" " + '-' + " ");
+					else
+						Console.Write(" " + numActivePlayersSegments[d * 24 + h] + " ");
+				}
+				Console.WriteLine();
+			}
 		}
 	}
 }
