5a94e4277573fa45b8d0974560fdd4771480c16e
Ash Mahein <ashdeep.mahein@wsu.edu>
2018-03-20 22:03:24 -0700

    added countdown

--- Code/Full Gamification/Assets/Conqueror/Scripts/CountdownScript.cs
+++ Code/Full Gamification/Assets/Conqueror/Scripts/CountdownScript.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CountdownScript : MonoBehaviour {
+namespace Conqueror {
+	public class CountdownScript : MonoBehaviour {
 
-    //Count down variable that is set to false while the counter is greater than 1.
-    public bool countDownDone = false;
+	    //Count down variable that is set to false while the counter is greater than 1.
+	    public bool countDownDone = false;
 
-	// Use this for initialization
-	void Start () {
+		// Use this for initialization
+		void Start () {
+			
+		}
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+		// Update is called once per frame
+		void Update () {
+			
+		}
 	}
 }
--- Code/Full Gamification/Assets/Conqueror/Scripts/setCountDown.cs
+++ Code/Full Gamification/Assets/Conqueror/Scripts/setCountDown.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This class sets the countdown to true when it hits 1.
+namespace Conqueror {
 public class setCountDown : MonoBehaviour {
-    private CountdownScript cs;
+	    private CountdownScript cs;
 
-    public void setCountdown()
-    {
-        cs = GameObject.Find("GameManager").GetComponent<CountdownScript>();
-        //countDownDone gets set to true when it is done counting down.
-        cs.countDownDone = true;
-    }
+		public void setCountdown()
+	    {
+	        cs = GameObject.Find("GameManager").GetComponent<CountdownScript>();
+	        //countDownDone gets set to true when it is done counting down.
+	        cs.countDownDone = true;
+	    }
+	}
 }
