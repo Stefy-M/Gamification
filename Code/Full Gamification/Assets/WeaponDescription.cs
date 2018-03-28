db0d8958f20783deb667ed954b21defc6dba1802
nhaney <nhaney@eecs.wsu.edu>
2018-03-27 21:26:31 -0700

    Created Weapon Description Screen
    Created weapon name description screen in conqueror, also removed max stamina button from incremental prefab.

---
+++ Code/Full Gamification/Assets/WeaponDescription.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDescription : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject startMenu;
+    public GameObject wepDescOverlay;
+    
+    public void displayWepDesc()
+    {
+        startMenu.SetActive(false);
+        wepDescOverlay.SetActive(true);
+    }
+
+    public void displayStartMenu()
+    {
+        startMenu.SetActive(true);
+        wepDescOverlay.SetActive(false);
+    }
+}
